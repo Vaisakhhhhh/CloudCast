9602b9de3aa3b1645977e081ab5f0dd9cee1a1de
Vaisakhhhhh <vaisakh4693@gmail.com>
2025-04-14 12:15:29 +0530

    first commit

---
+++ src/pages/weather-dashboard.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button"
+import { useGeolocation } from "@/hooks/use-geolocation";
+import { RefreshCcw } from "lucide-react"
+
+
+const WeatherDashboard = () => {
+    const { coordinates, error, getLocation, isLoading} = useGeolocation();
+
+
+    console.log(coordinates);
+    
+
+  return (
+    <div className="space-y-4">
+      {/* {Favorite Cities} */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button variant={"outline"}
+          size={"icon"}
+        //   onClick={handleRefresh}
+        //   disabled={}
+          >
+            <RefreshCcw className="h-4 w-4"/>
+        </Button>
+      </div>
+
+      {/* {Current and Hourly weather} */}
+    </div>
+  );
+};
+
+export default WeatherDashboard
